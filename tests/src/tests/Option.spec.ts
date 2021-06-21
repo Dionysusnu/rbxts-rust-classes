44b32e3993272ad2bd291025035d2f20817a6a1b
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-06-21 15:58:36 +0200

    Add first tests

--- tests/src/tests/Option.spec.ts
+++ tests/src/tests/Option.spec.ts
@@ -3,7 +3,16 @@
 import { Option } from "@rbxts/rust-classes";
 
 export = () => {
-	describe("Option.isSome", () => {
-		expect(Option.some(1).isSome()).to.be.ok();
+	it("Option.isSome", () => {
+		expect(Option.some(1).isSome()).to.equal(true);
+		expect(Option.none().isSome()).to.equal(false);
+		expect(Option.wrap(1).isSome()).to.equal(true);
+		expect(Option.wrap(undefined).isSome()).to.equal(false);
+	});
+	it("Option.isNone", () => {
+		expect(Option.some(1).isNone()).to.equal(false);
+		expect(Option.none().isNone()).to.equal(true);
+		expect(Option.wrap(1).isNone()).to.equal(false);
+		expect(Option.wrap(undefined).isNone()).to.equal(true);
 	});
 };
