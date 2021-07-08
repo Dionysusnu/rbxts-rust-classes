997854839c8b41208323bb9692bc286d9bc69d2f
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-07-08 17:15:10 +0200

    Add unit type unit tests

---
+++ tests/src/tests/Unit.spec.ts
@@ -0,0 +1,14 @@
+/// <reference types="@rbxts/testez/globals" />
+
+import { unit } from "@rbxts/rust-classes";
+
+export = () => {
+	it("Unit.__eq", () => {
+		expect(unit()).to.equal(unit());
+		expect(unit()).never.to.equal(1);
+		expect(1).never.to.equal(unit());
+	});
+	it("Unit.__index", () => {
+		expect(() => (unit() as { index: unknown }).index).to.throw();
+	});
+};
